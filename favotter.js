fc5b394ae336a433a4b8f7e4f1e1715b0cc15d44
NeoCat <NeoCat@d0d07461-0603-4401-acd4-de1884942a52>
2008-09-27 07:27:58 +0000

    add popup menu for each message
    git-svn-id: http://svn.coderepos.org/share/websites/twicli@20041 d0d07461-0603-4401-acd4-de1884942a52

--- favotter.js
+++ favotter.js
@@ -5,7 +5,7 @@ var twFavPlugin = {
 	update: function() {
 		if (this.cnt++ < 5) return;
 		this.cnt = 0;
-		this.fav_update = loadXDomainScript('nr_favs.js?seq='+(seq++), this.fav_update);
+		this.fav_update = loadXDomainScript('http://www.geocities.jp/twicli/nr_favs.js?seq='+(seq++), this.fav_update);
 	},
 	newMessageElement: function(ele, tw) {
 		var fele = document.createElement("a");
@@ -14,11 +14,16 @@ var twFavPlugin = {
 		fele.target = "twitter";
 		if (this.favs[tw.id]) {
 			fele.style.fontSize = "small";
-			fele.style.color = "#092";
+			fele.style.backgroundColor = "#3fc";
 			fele.innerHTML = '[fav:' + this.favs[tw.id] + '] ';
 		}
 		ele.insertBefore(fele, ele.childNodes[4]);
 	},
+	popup: function(ele, user, id) {
+		$('favotter_link_user').href = 'http://favotter.matope.com/user.php?user=' + user;
+		$('favotter_link_status').href = 'http://favotter.matope.com/status.php?id=' + id;
+		$('favotter_link_fav').href = 'http://favotter.matope.com/user.php?mode=fav&user=' + user;
+	}
 };
 registerPlugin(twFavPlugin);
 
@@ -30,3 +35,28 @@ function favEntries(favs) {
 			target.innerHTML = '[fav:' + favs[x] + '] '
 	}
 }
+
+// Popup menu
+
+var a = document.createElement("hr");
+$('popup').appendChild(a)
+
+a = document.createElement("a");
+a.target = 'twitter';
+a.id = 'favotter_link_user';
+a.innerHTML = 'ふぁぼったー / ふぁぼられ';
+$('popup').appendChild(a)
+
+a = document.createElement("a");
+a.target = 'twitter';
+a.id = 'favotter_link_status';
+a.innerHTML = 'ふぁぼったー / 発言';
+$('popup').appendChild(a)
+
+a = document.createElement("a");
+a.target = 'twitter';
+a.id = 'favotter_link_fav';
+a.innerHTML = 'ふぁぼったー / ふぁぼり';
+$('popup').appendChild(a)
+
+
