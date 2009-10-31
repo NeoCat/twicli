67835726dd0514783fd9c78e5db8ac7a199459b1
Atsushi Takayama <atsushi@utopia.local>
2009-10-31 03:29:09 -0400

    don't send request to expand already-shortened URL

--- shorten_url.js
+++ shorten_url.js
@@ -1,21 +1,16 @@
 /* assist inline URL shortening in the text area */
 (function(){
   var wait = 2000; // time to wait before deciding that the server is unreacheable
-
   var _updateCount = window.updateCount;
   var fst = $('fst');
   var timer, ele, command, original;
   var remove = function(e){if(e && e.parentNode)e.parentNode.removeChild(e); e = null};
+  var api = 'http://atsushaa.appspot.com/shorten/get';
+  var cache = {};
 
   window.replaceText = function(hash){
-    var replacement = hash[original];
-    if (replacement) {
-      try{ // unescape percent-encoded UTF-8 characters
-        replacement = decodeURI(replacement);
-      }catch(e){}
-    } else {
-      replacement = original;
-    }
+    var replacement = hash[original] || original;
+    cache[replacement] = original;
 
     if (fst.value.indexOf(command) >= 0) {
       fst.value = fst.value.replace(command, replacement);
@@ -31,17 +26,19 @@
     if (!(ele && ele.parentNode) && /((https?:\/\/\S+?)(?:;;;|；；；))/.test(fst.value)) {
       command = RegExp.$1;
       original = RegExp.$2;
-      if (original.indexOf('http://j.mp/') === 0 || original.indexOf('http://bit.ly/') === 0) {
-        var src = 'http://atsushaa.appspot.com/untiny/get?callback=replaceText&url='+encodeURIComponent(original);
+      if (cache[original]) {
+        timer = setTimeout(function(){ window.replaceText(cache); }, 100);
       } else {
-        var src = 'http://atsushaa.appspot.com/shorten/get?callback=replaceText&url='+encodeURIComponent(original);
+        var src = api + '?callback=replaceText&url='+encodeURIComponent(original);
+        ele = loadXDomainScript(src);
+        timer = setTimeout(function(){ window.replaceText({original:null}); }, wait);
       }
-      ele = loadXDomainScript(src, ele);
-      timer = setTimeout(function(){
-        window.replaceText({original:null});
-      }, wait);
     }
     _updateCount();
   }
+
+  registerPlugin({
+    post: function(){ cache = {} }
+  })
 })();
 
