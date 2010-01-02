b9a0fec9310fac623f27b71db58aaf0436974f5f
NeoCat <NeoCat@d0d07461-0603-4401-acd4-de1884942a52>
2010-01-02 17:55:36 +0000

    Avoid post targetting inline-frame fails with ssl.js in Opera
    git-svn-id: http://svn.coderepos.org/share/websites/twicli@36335 d0d07461-0603-4401-acd4-de1884942a52

--- ssl.js
+++ ssl.js
@@ -2,3 +2,14 @@ twitterURL = 'https://twitter.com/';
 twitterAPI = 'https://api.twitter.com/1/';
 document.frm.action = 'https://api.twitter.com/1/statuses/update.xml';
 document.frm.target = 'tx';
+
+var origResetFrm;
+if (navigator.userAgent.indexOf("Opera") >= 0) {
+	origResetFrm = resetFrm;
+	resetFrm = function() { // Opera fails to post targetting https frame
+		try { if (document.frames.tx.location.href == "about:blank") return; } catch(e) {
+			document.frames.tx.location.href = "about:blank";
+			origResetFrm();
+		}
+	}
+}
