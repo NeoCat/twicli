9c3c6c59139bb7d22e6333cde9648798f79ffe21
NeoCat <neocat@neocat.jp>
2010-09-25 06:50:33 +0000

    change zoom level to 13 in geomap.js
    git-svn-id: http://svn.coderepos.org/share/websites/twicli@38519 d0d07461-0603-4401-acd4-de1884942a52

--- plugins/geomap.js
+++ plugins/geomap.js
@@ -37,7 +37,7 @@ function display_map(coordinates, elem) {
 function make_geo_map(coordinates) {
 	var latlng = new google.maps.LatLng(coordinates[0], coordinates[1]);
 	var map = new google.maps.Map(document.getElementById("map_canvas"),
-		{zoom: 10, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP});
+		{zoom: 13, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP});
 	var marker = new google.maps.Marker({position: latlng, map: map});
 	google.maps.event.addListener(marker, 'click', function(event) {
 		window.open('http://maps.google.com?q='+coordinates.join(","));
