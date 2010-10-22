3c0f51e824b434b093e26a96bdc71a65728fecda
Takashi WADA <twada@amy.hi-ho.ne.jp>
2010-10-23 00:25:49 +0900

    use switchTo() function's argument, instead of document.getElementsByClassName("sel")[0]

--- plugins/insert_hashtag.js
+++ plugins/insert_hashtag.js
@@ -2,17 +2,12 @@
 	var footerSetting = footer;
 
 	registerPlugin({
-		switchTo: function(m) {
-			var sel = document.getElementsByClassName("sel");
-
-			if(sel.length < 1) return;
-
-			if(sel[0].id.indexOf("search-#") != 0) {
+		switchTo: function(menu) {
+			if(menu.id.indexOf("search-#") != 0) {
 				footer = footerSetting;
 				return;
 			}
-
-			footer = " " + sel[0].id.slice("search-".length);
+			footer = " " + menu.id.slice("search-".length);
 			if(footerSetting.length > 0) footer +=" " + footerSetting;
 		},
 		savePrefs: function(frm) {
