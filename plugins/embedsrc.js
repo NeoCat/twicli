57464031516e9adb83239546f8c24f1a9ff0630c
Takashi WADA <twada@amy.hi-ho.ne.jp>
2011-06-19 22:41:00 +0900

    embedsrc.js: Add SlideShare

--- plugins/embedsrc.js
+++ plugins/embedsrc.js
@@ -4,6 +4,15 @@
 		{search: /^https?:\/\/raw\.github\.com\/gist\/(\d+)(?:.*)$/, replace: "https://gist.github.com/$1.pibb"}
 	];
 
+	var createAnchor = function(link, onclick) {
+		var a = document.createElement('a');
+		a.className = "button";
+		a.href = "#";
+		a.onclick = onclick;
+		a.innerHTML = '<img src="images/jump.png" alt="☞" width="14" height="14">';
+		link.parentNode.insertBefore(a, link.nextSibling);
+	};
+
 	registerPlugin({
 		newMessageElement: function(elem) {
 			var status = null;
@@ -20,22 +29,26 @@
 			}
 		},
 		replaceUrl : function(elem, link, lng, sht) {
-			var div, a;
 			for (var i = 0; i < res.length; i++) {
 				if (res[i].search.test(lng)) {
-					a = document.createElement('a');
-					a.className = "button";
-					a.href = "#";
-					a.onclick = function(){
+					createAnchor(link, function(){
 						dispEmbedSrc(lng.replace(res[i].search, res[i].replace), link);
 						return false;
-					};
-					a.innerHTML = '<img src="images/jump.png" alt="☞" width="14" height="14">';
-					div = document.createElement('div');
-					div.appendChild(a);
-					link.parentNode.insertBefore(div.firstChild, link.nextSibling);
-					break;
+					});
+					return;
 				}
+			}
+			if (lng.match(/^(https?:\/\/www\.slideshare\.net\/[-_0-9a-zA-Z]+\/[-_0-9a-zA-Z]+)/)) {
+				xds.load("http://www.slideshare.net/api/oembed/2?url=" + RegExp.$1 + "&format=jsonp",
+						function(x) {
+							createAnchor(link, function(){
+								dispEmbedSrc("http:\/\/www\.slideshare\.net\/slideshow\/embed_code\/"
+									+ x.slideshow_id,
+									link);
+								return false;
+							});
+							
+						});
 			}
 		}
 	});
@@ -46,7 +59,7 @@ function dispEmbedSrc(url, link) {
 
 	$('rep').style.display = "block";
 	$('reps').innerHTML = '<iframe id="embedsrc" src="' + url
-		+ '" style="width:100%; height: 250px; display:block">'
+		+ '" style="width:100%; height: 250px; display:block">';
 	$('rep').style.top = rep_top;
 	user_pick1 = user_pick2 = null;
 }
