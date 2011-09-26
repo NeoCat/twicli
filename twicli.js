548b648c062abb4e99a6648d13c6a49929166d5c
NeoCat <neocat@neocat.jp>
2011-09-26 14:04:58 +0000

    Show retweet_count
    git-svn-id: http://svn.coderepos.org/share/websites/twicli@39030 d0d07461-0603-4401-acd4-de1884942a52

--- twicli.js
+++ twicli.js
@@ -846,7 +846,7 @@ function makeHTML(tw, no_name, pid) {
 			}).replace(/\r?\n|\r/g, "<br>") + '</span>' +
 		//Retweet情報
 		' <span id="rtinfo-'+eid+'" class="rtinfo">' +
-		(!display_as_rt && rt ? "<img src=\"images/rt.png\" alt=\"RT\">by <img src=\""+tw.user.profile_image_url+"\" alt=\""+tw.user.screen_name+"\" class=\"rtuicon\"><a href=\""+twitterURL+tw.user.screen_name+"\" onclick=\"switchUserTL(this.parentNode.parentNode, true);return false\">" + tw.user.screen_name + "</a> " :'') + '</span>' +
+		(!display_as_rt && rt ? "<img src=\"images/rt.png\" alt=\"RT\">by <img src=\""+tw.user.profile_image_url+"\" alt=\""+tw.user.screen_name+"\" class=\"rtuicon\"><a href=\""+twitterURL+tw.user.screen_name+"\" onclick=\"switchUserTL(this.parentNode.parentNode, true);return false\">" + tw.user.screen_name + "</a> " + (parseInt(tw.retweet_count) > 1 ? '<small>& ' + tw.retweet_count + '</small>' : '') :'') + '</span>' +
 		//日付
 		' <span id="utils-'+eid+'" class="utils">' +
 		'<span class="prop"><a class="date" target="twitter" href="'+twitterURL+(t.d_dir ? '#!/messages' : un+'/statuses/'+id2)+'">' + dateFmt(t.created_at) + '</a>' +
