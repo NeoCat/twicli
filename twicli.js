ba20a9a7328fb8c58c362176c7e48d978fd34636
NeoCat <neocat@neocat.jp>
2012-02-15 01:48:41 +0000

    Remove non-alphabetical characters from URL tail
    git-svn-id: http://svn.coderepos.org/share/websites/twicli@39114 d0d07461-0603-4401-acd4-de1884942a52

--- twicli.js
+++ twicli.js
@@ -842,7 +842,7 @@ function makeHTML(tw, no_name, pid, userdesc) {
 		text.replace(/https?:\/\/[^\/\s]*[\w!#$%&'()*+,.\/:;=?@~-]+(?=&\w+;)|https?:\/\/[^\/\s]*[\w!#$%&'()*+,.\/:;=?@~-]+|[@＠](\w+(?:\/[\w-]+)?)|([,.!?　、。！？「」]|\s|^)([#＃])([\w々ぁ-ゖァ-ヺーㄱ-ㆅ㐀-\u4DBF一-\u9FFF가-\uD7FF\uF900-\uFAFF０-９Ａ-Ｚａ-ｚｦ-ﾟ]+)(?=[^\w々ぁ-ゖァ-ヺーㄱ-ㆅ㐀-\u4DBF一-\u9FFF가-\uD7FF\uF900-\uFAFF０-９Ａ-Ｚａ-ｚｦ-ﾟ]|$)/g, function(_,u,x,h,s){
 				if (!u && !h) {
 					var paren = '';
-					_ = _.replace(/[()]+$/,function(p){ // 末尾の"()"はURLに含めない
+					_ = _.replace(/[\W_]+$/,function(p){ // 末尾の記号類はURLに含めない
 						paren = p;
 						return '';
 					});
