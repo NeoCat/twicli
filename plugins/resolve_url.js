8cc629bf26e5aac74dc2517f171994e36bc13eaf
NeoCat <neocat@neocat.jp>
2012-10-04 01:52:42 +0000

    resolve_url.js: add some tinyurl services; use resolve-url.appspot.com as resolver API
    git-svn-id: http://svn.coderepos.org/share/websites/twicli@39191 d0d07461-0603-4401-acd4-de1884942a52

--- plugins/resolve_url.js
+++ plugins/resolve_url.js
@@ -1,7 +1,7 @@
 /* replace short URLs with original URLs */
 (function(){
-  var re = /^http:\/\/(t\.co|tinyurl\.com|bit\.ly|is\.gd|u\.nu|icio\.us|tr\.im|cli\.gs|twurl\.nl|url\.ie|j\.mp|ow\.ly|ff\.im|digg\.com|dlvr.it|(?:www\.)?tumblr\.com|tmblr\.co|htn\.to|goo\.gl|slidesha\.re)\/|http:\/\/p\.tl\/(?!.\/)/;
-  var api = 'http://atsushaa.appspot.com/untiny/get';
+  var re = /^http:\/\/(t\.co|tinyurl\.com|bit\.ly|is\.gd|u\.nu|icio\.us|tr\.im|cli\.gs|twurl\.nl|url\.ie|j\.mp|ow\.ly|ff\.im|digg\.com|dlvr.it|buff\.ly|po\.st|(?:www\.)?tumblr\.com|tmblr\.co|htn\.to|goo\.gl|slidesha\.re)\/|http:\/\/p\.tl\/(?!.\/)/;
+  var api = 'http://resolve-url.appspot.com/url';
   var remove = function(e){if (e && e.parentNode) e.parentNode.removeChild(e)};
 
   window.replaceUrl = function(hash, link) {
