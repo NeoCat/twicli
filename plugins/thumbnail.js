a7f71f8b30b0348a4ea0da328eb2fe798ec13629
NeoCat <neocat@neocat.jp>
2013-03-29 00:02:56 -0400

    thumbnail.js: instagram.com

--- plugins/thumbnail.js
+++ plugins/thumbnail.js
@@ -93,7 +93,7 @@ registerPlugin({
 			else
 				addThumbnail(elem, 'http://tn-skr' + host + '.smilevideo.jp/smile?i=' + id, url);
 		}
-		else if (url.match(/^(http:\/\/instagr\.am\/p\/[\w\-]+)\/?$/)) {
+		else if (url.match(/^(http:\/\/(?:instagr\.am|instagram\.com)\/p\/[\w\-]+)\/?$/)) {
 			addThumbnail(elem, RegExp.$1+'/media/?size=t', url);
 		}
 		else if (url.match(/^(http:\/\/picplz.com\/\w+)/)) {
