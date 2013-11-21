8f8d476582f08022d3e8e84e35f42d5d0421e5d0
NeoCat <neocat@neocat.jp>
2013-11-21 12:48:36 -0500

    tweet_url_reply.js: overlay photo by clicking > button of twitter photo URL

--- plugins/tweet_url_reply.js
+++ plugins/tweet_url_reply.js
@@ -1,3 +1,11 @@
+function dispImageFromLink(url, e) {
+  if (e.parentNode.parentNode.parentNode.id != 'reps') rep_top = cumulativeOffset(e)[1] + 20;
+  $('reps').innerHTML = '<img src="' + url + '" style="max-width:90%; max-height: 90%; margin: auto; display: block;">';
+  $('rep').style.display = 'block';
+  $('rep').style.top = rep_top + 'px';
+  scrollToDiv($('reps'));
+}
+
 (function() {
   var re = /^https?:\/\/twitter\.com\/(?:#!\/|#%21\/)?(\w+)\/status(?:es)?\/(\d+)/;
 
@@ -20,7 +28,12 @@
     if (m) {
       a.tweetUrlChecked = true;
       var dummy = document.createElement('div');
-      dummy.innerHTML = '<a class="button" href="#" onClick="dispReply(\'' + m[1] + '\',\'' + m[2] + '\',this); return false;"><img src="images/jump.png" alt="☞" width="14" height="14"></a>';
+      var script = 'dispReply(\'' + m[1] + '\',\'' + m[2] + '\',this); return false;';
+      var tw = a.parentNode.parentNode.tw;
+      tw = tw.retweeted_status ? tw.retweeted_status : tw;
+      if (tw && tw.user.screen_name == m[1] && tw.id_str == m[2] && a.href.indexOf('/photo/1') >= 0)
+        script = 'dispImageFromLink(\'' + tw.entities.media[0].media_url + ':medium\', this); return false;';
+      dummy.innerHTML = '<a class="button" href="#" onClick="' + script + '"><img src="images/jump.png" alt="☞" width="14" height="14"></a>';
       a.parentNode.insertBefore(dummy.firstChild, a.nextSibling);
     }
   }
