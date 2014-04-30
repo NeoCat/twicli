5744aec63264669c656d6de47963e88e0d1289bd
NeoCat <neocat@neocat.jp>
2014-04-29 23:38:25 -0400

    add find_rt.js to find out tweet targetted by '>RT'

---
+++ plugins/find_rt.js
@@ -0,0 +1,47 @@
+(function(){
+	registerPlugin({
+		newMessageElement: function(elem) {
+			var node = elem;
+			var status = null;
+			for(var i = 0; i < elem.childNodes.length; i++) {
+				status = elem.childNodes[i];
+				if (status.className && status.className.indexOf('status') >= 0)
+					break;
+			}
+			for (var i = 0; i < status.childNodes.length; i++) {
+				var ele = status.childNodes[i];
+				if (ele.nodeType == 3) {// text node
+					if (ele.nodeValue.match(/([\s\S]*)((?:>|＞|→)\s*RT)([\s\S]*)/)) {
+						ele.nodeValue = RegExp.$1;
+						var a = document.createElement('a');
+						a.href = "#";
+						a.onclick = function(){
+							var p = elem;
+							var pp = elem.parentNode;
+							while (true) {
+								if (!p) {
+									pp = pp.nextSibling;
+									if (!pp) break;
+									p = pp.childNodes[0];
+								} else {
+									p = p.nextSibling;
+								}
+								if (!p || !p.tw) continue;
+								if (p.tw.user.id == (node.tw.retweeted_status||node.tw).user.id && p.tw.retweeted_status) {
+									rep_top = cumulativeOffset(node)[1] + 20;
+									dispReply2(p.tw);
+									break;
+								}
+							}
+							return false;
+						};
+						a.appendChild(document.createTextNode(RegExp.$2));
+						status.insertBefore(a, ele.nextSibling);
+						status.insertBefore(document.createTextNode(RegExp.$3), ele.nextSibling);
+					}
+				}
+			}
+		}
+	});
+}());
+
