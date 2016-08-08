528875eb5f64e93105c677dbb77abc719a89f75e
NeoCat <neocat@neocat.jp>
2016-08-08 20:17:56 +0900

    Add ioc_flags.js by default

--- twicli.js
+++ twicli.js
@@ -429,7 +429,7 @@ var myid = null;		// 自ユーザID
 var last_user = null;	// user TLに表示するユーザ名
 var last_user_info = null;	// user TLに表示するユーザ情報(TLから切替時のキャッシュ)
 // 設定値
-var currentCookieVer = 22;
+var currentCookieVer = 23;
 var cookieVer = parseInt(readCookie('ver')) || 0;
 var updateInterval = (cookieVer>18) && parseInt(readCookie('update_interval')) || 90;
 var pluginstr = (cookieVer>6) && readCookie('tw_plugins') || ' regexp.js\nlists.js\nsearch.js\nfollowers.js\nshorten_url.js\nresolve_url.js';
@@ -449,6 +449,7 @@ if (cookieVer<18) if (pluginstr.indexOf('shortcutkey.js')<0) pluginstr+="\nshort
 if (cookieVer<18) if (pluginstr.indexOf('multi_account.js')<0) pluginstr+="\nmulti_account.js";
 if (cookieVer<18) if (pluginstr.indexOf('notify.js')<0) pluginstr+="\nnotify.js";
 if (cookieVer<21) if (pluginstr.indexOf('tweets_after_rt.js')<0) pluginstr+="\ntweets_after_rt.js";
+if (cookieVer<23) if (pluginstr.indexOf('ioc_flags.js')<0) pluginstr+="\nioc_flags.js";
 pluginstr = pluginstr.substr(1);
 var plugins = new Array;
 var max_count = Math.min((cookieVer>3) && parseInt(readCookie('max_count')) || 50, 800);
