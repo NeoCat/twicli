62d4f5dcf7bbdb6c9edccd0836f1d8de3953bebb
NeoCat <neocat@neocat.jp>
2019-01-09 01:05:41 +0900

    update copyright year

--- twicli.js
+++ twicli.js
@@ -1866,7 +1866,7 @@ function switchDirect() {
 }
 function switchMisc() {
 	switchTo("misc");
-	$("tw2h").innerHTML = '<br><a id="clientname" target="twitter" href="index.html"><b>twicli</b></a> : A browser-based Twitter client<br><small id="copyright">Copyright &copy; 2008-2015 NeoCat</small><hr class="spacer">' +
+	$("tw2h").innerHTML = '<br><a id="clientname" target="twitter" href="index.html"><b>twicli</b></a> : A browser-based Twitter client<br><small id="copyright">Copyright &copy; 2008-2019 NeoCat</small><hr class="spacer">' +
 	'<a href="javascript:showMediaOption()">' + _('Upload images') + '</a><br>' +
 					'<form id="switchuser" onSubmit="switchUser($(\'user_id\').value); return false;">'+
 					_('show user info')+' : @<input type="text" size="15" id="user_id" value="' + myname + '"><button type="submit" class="go"></button></form>' +
